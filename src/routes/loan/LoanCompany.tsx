1aee6035d2417c402b78ac0c17363533f86a849d
김영석 <kimyoungseok15@gmail.com>
2024-06-05 15:28:37 +0900

    feat: 각 페이지 별 기본 파일 생성

---
+++ src/routes/loan/LoanCompany.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LoanCompany() {
+  return (
+    <div>LoanCompany</div>
+  )
+}
---
+++ src/routes/user/LoginKakao.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LoginKakao() {
+  return (
+    <div>LoginKakao</div>
+  )
+}
