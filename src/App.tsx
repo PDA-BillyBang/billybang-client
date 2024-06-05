adbfcbc2dd9ccce94fabedad8997e2347fcc0907
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-06-05 15:38:45 +0900

    feat : navigate button 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import LargeButton from "./components/common/button/LargeButton";
+import NavigateButton from "./components/common/button/NavigateButton";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -11,6 +12,11 @@ function App() {
       <LargeButton text="확인" customWidth="w-[80vw]" isActive={1} />
       <div className="my-2" />
       <LargeButton text="확인" customWidth="w-[80vw]" isActive={2} />
+      <div className="my-2" />
+      <NavigateButton
+        text="나에게 맞는 대출 상품이 궁금하다면 로그인하기"
+        customWidth="w-[80vw]"
+      />
     </>
   );
 }
