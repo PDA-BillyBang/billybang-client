cc7ec8bd28a69807666dfcb40f1a8e2ec0559be7
김영석 <kimyoungseok15@gmail.com>
2024-06-10 13:35:57 +0900

    feat: 메인페이지 지도 로드 성공

--- src/routes/map/Map.tsx
+++ src/routes/map/Map.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useEffect} from "react";
 
-export default function Map() {
+type Props = {};
+
+export default function Map({}: Props) {
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new window.kakao.maps.LatLng(37.5665, 126.9780), // 지도의 중심좌표
+      level: 3 // 지도의 확대 레벨
+    };
+    const map = new window.kakao.maps.Map(container, options); // 지도를 생성
+  }, []);
+
   return (
-    <div>Map</div>
-  )
+    <div id="map" className="h-96 w-customWidthPercent bg-grey-6 rounded-[5px]">
+      Map
+    </div>
+  );
 }
