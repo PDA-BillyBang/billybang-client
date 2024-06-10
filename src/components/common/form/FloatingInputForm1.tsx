97d884550bd74bda5dd00cb1732d26719497165e
sunyoung <4446207@naver.com>
2024-06-11 08:53:00 +0900

    feat : 로그인 패스워드 입력 페이지 구현

--- src/components/common/form/FloatingInputForm1.tsx
+++ src/components/common/form/FloatingInputForm1.tsx
@@ -1,14 +1,33 @@
-import { FloatingLabel } from "flowbite-react";
-import React, { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 
 type Props = {
+  type?: string;
   title?: string;
   text: string;
+  value: string; // 입력 값을 부모로부터 받음
+  onChange: (value: string) => void; // 입력 값이 변경될 때 호출할 핸들러
+  validate?: (value: string) => boolean; // 유효성 검사 함수
+  errorMessage?: string; // 오류 메시지
 };
 
-export default function InputForm({ title, text }: Props) {
-  // 포커스 상태를 관리하는 상태 변수 선언
+export default function FloatingInputForm1({
+  type = "text",
+  title,
+  text,
+  value,
+  onChange,
+  validate,
+  errorMessage,
+}: Props) {
   const [isFocused, setIsFocused] = useState(false);
+  const [isValid, setIsValid] = useState(true);
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    if (validate) {
+      setIsValid(validate(value));
+    }
+  };
 
   return (
     <div className="w-full flex flex-col">
@@ -20,12 +39,19 @@ export default function InputForm({ title, text }: Props) {
         {title}
       </div>
       <input
-        type="text"
+        type={type}
         placeholder={text}
-        onFocus={() => setIsFocused(true)} // 포커스 이벤트 발생 시 포커스 상태를 true로 변경
-        onBlur={() => setIsFocused(false)} // 포커스가 해제될 때 포커스 상태를 false로 변경
-        className="w-full h-[3rem] border-b border-grey-2 placeholder-grey-2 w-customWidthPercent focus:outline-none focus:border-[black]"
+        value={value}
+        onFocus={() => setIsFocused(true)}
+        onBlur={handleBlur}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          onChange(e.target.value)
+        }
+        className="w-full h-[3rem] border-b border-grey-2 placeholder-grey-2 focus:outline-none focus:border-[black]"
       />
+      {!isValid && (
+        <div className="text-blue-1 text-[0.8rem] mt-1">{errorMessage}</div>
+      )}
     </div>
   );
 }
--- src/routes/user/Signup.tsx
+++ src/routes/user/Signup.tsx
@@ -1,20 +1,22 @@
 import ProgressBar from "../../components/common/progressbar/ProgressBar";
-import React, {useEffect} from 'react';
-import { useOutletContext } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function SignUp() {
-  const { setTitle } = useOutletContext<{ setTitle: (title: string) => void }>();
+  const { setTitle } = useOutletContext<{
+    setTitle: (title: string) => void;
+  }>();
 
   useEffect(() => {
-    setTitle('회원가입');
+    setTitle("회원가입");
   }, [setTitle]);
 
   return (
     <>
       <div className="flex flex-nowrap">
-        <ProgressBar color="blue-1" /> {/* Tailwind의 blue-900 컬러 */}
-        <ProgressBar color="grey-2" /> {/* Tailwind의 gray-200 컬러 */}
-        <ProgressBar color="grey-2" /> {/* Tailwind의 gray-200 컬러 */}
+        <ProgressBar color={0} /> {/* Tailwind의 blue-900 컬러 */}
+        <ProgressBar color={1} /> {/* Tailwind의 gray-200 컬러 */}
+        <ProgressBar color={2} /> {/* Tailwind의 gray-200 컬러 */}
       </div>
     </>
   );
