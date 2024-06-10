995d7d2b38043ca26f22e7f4c622487bdde1a02b
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-06-10 14:31:30 +0900

    feat: bottom drawer 추가

--- src/routes/loan/Loan.tsx
+++ src/routes/loan/Loan.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import LoanCard from "../../components/loan/LoanCard";
 import LoanHeader from "../../components/loan/LoanHeader";
+import LoanFiltering from "../../components/loan/LoanFiltering";
+import BottomDrawer from "../../components/common/button/BottomDrawer";
 
 const Loan = () => {
   const { setTitle } = useOutletContext<{
     setTitle: (title: string) => void;
   }>();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => setIsOpen((prev) => !prev);
 
   useEffect(() => {
     setTitle("충무로엘크루메크로시티2"); // 실제로는 api로 propertyId에 해당하는 제목을 받아와서 갈아끼우기
@@ -14,6 +20,10 @@ const Loan = () => {
 
   return (
     <div className="flex flex-col items-center">
+      <LoanFiltering handleClick={handleClick} />
+      <BottomDrawer isOpen={isOpen} handleClose={handleClick}>
+        <div>children</div>
+      </BottomDrawer>
       <LoanHeader />
       <div className="py-2"></div>
       <LoanCard />
