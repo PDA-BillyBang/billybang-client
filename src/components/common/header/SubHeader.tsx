ed7e99f99b10ed9e8db59f727005e0e648818917
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-06-11 08:49:19 +0900

    feat: subheader 뒤돌아가기 라우트 추가

--- src/components/common/header/SubHeader.tsx
+++ src/components/common/header/SubHeader.tsx
@@ -4,19 +4,25 @@ import { Navbar } from "flowbite-react";
 import backButton from "../../../assets/image/icons/backButton.svg";
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SubHeader() {
   const [title, setTitle] = useState("");
+  const navigate = useNavigate();
+  const handleClickBack = () => {
+    navigate(-1);
+  };
 
   return (
     <div>
       <Navbar fluid rounded>
         <div className="flex items-center justify-between w-full">
-          <Navbar.Brand href="https://flowbite-react.com">
+          <Navbar.Brand>
             <img
               src={backButton}
               className="h-5 sm:h-9 size-7 "
               alt="Billibang Logo"
+              onClick={handleClickBack}
             />
           </Navbar.Brand>
           <span className="self-center mx-auto text-xl font-semibold">
