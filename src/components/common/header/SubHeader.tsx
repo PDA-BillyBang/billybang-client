81bb9b8e67293df181edcc62b64d467bb956cda1
sunyoung <4446207@naver.com>
2024-06-13 08:46:43 +0900

    feat : 정보입력 2,3페이지 구현 완료

--- src/components/common/header/SubHeader.tsx
+++ src/components/common/header/SubHeader.tsx
@@ -14,9 +14,9 @@ export default function SubHeader() {
   };
 
   return (
-    <div className="bg-white-1">
-      <Navbar fluid rounded>
-        <div className="fixed top-0 pt-[0.5rem] bg-white-1 flex items-center justify-between w-full">
+    <div className="bg-white-1 ">
+      <Navbar className="fixed top-0 w-full">
+        <div className="pt-[0.5rem] bg-white-1 flex items-center justify-between w-full">
           <Navbar.Brand>
             <img
               src={backButton}
@@ -25,12 +25,7 @@ export default function SubHeader() {
               onClick={handleClickBack}
             />
           </Navbar.Brand>
-          {/* <span className="self-center mx-auto text-xl font-semibold">
-            {title}
-          </span> */}
-          {/* <Navbar.Brand href="https://flowbite-react.com">
-            <div className="h-5 sm:h-9 size-7 "></div>
-          </Navbar.Brand> */}
+
           <div className="flex-1 text-center">
             <span className="text-xl font-semibold">{title}</span>
           </div>
