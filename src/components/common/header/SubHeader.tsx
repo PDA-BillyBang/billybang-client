a088b15edfc952ba0c468fdcf2e4f3a3be3ca5ed
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-06-13 08:38:28 +0900

    feat: route key error 수정

--- src/components/common/header/SubHeader.tsx
+++ src/components/common/header/SubHeader.tsx
@@ -31,7 +31,7 @@ export default function SubHeader() {
           <div className="h-5 sm:h-9 size-4"></div>
         </Navbar>
       </div>
-      <div className="pt-[2.5rem]" />
+      <div className="my-[30px]" />
       <Outlet context={{ setTitle }} />
     </div>
   );
--- src/routes/mypage/Mypage.tsx
+++ src/routes/mypage/Mypage.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import profileTest from "../../assets/image/test/profile-test.svg";
 
 export default function Mypage() {
   return (
-    <div>Mypage</div>
-  )
+    <div className="w-customWidthPercent">
+      <div className="flex flex-row items-center">
+        <div className="w-[30%]">
+          <img
+            src={profileTest}
+            alt="profile-test"
+            className="h-[58px] w-[58px]"
+          />
+        </div>
+        <div className="flex flex-col w-[70%]">
+          <div className="font-bold text-[1rem]">김영석</div>
+          <div className="text-[0.8rem] text-grey-1">
+            kimyoungseok15@gmail.com
+          </div>
+        </div>
+      </div>
+      <div>찜한 방</div>
+    </div>
+  );
 }
