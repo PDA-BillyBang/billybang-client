9d74ab1e48b7c73f29697187f38a2c8ba97e2484
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-06-18 10:48:33 +0900

    feat: header margin 통일

--- src/components/common/header/SubHeader.tsx
+++ src/components/common/header/SubHeader.tsx
@@ -1,13 +1,13 @@
-"use client";
-import React from "react";
-import { Navbar } from "flowbite-react";
-import backButton from "../../../assets/image/icons/backButton.svg";
-import { Outlet } from "react-router-dom";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+'use client';
+import React from 'react';
+import { Navbar } from 'flowbite-react';
+import backButton from '../../../assets/image/icons/backButton.svg';
+import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SubHeader() {
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState('');
   const navigate = useNavigate();
   const handleClickBack = () => {
     navigate(-1);
@@ -15,21 +15,21 @@ export default function SubHeader() {
 
   return (
     <div className="flex flex-col ">
-      <div className="fixed top-0 pt-[0.5rem] w-full bg-white-1 z-10">
-        <Navbar className="flex flex-row justify-between w-full bg-white-1">
+      <div className="fixed h-16 top-0 pt-[0.5rem] w-full bg-white-1 z-10">
+        <div className="flex flex-row items-center justify-between w-full px-3 bg-white-1">
           <div>
             <img
               src={backButton}
-              className="h-5 cursor-pointer sm:h-9 size-4"
-              alt="Billibang Logo"
+              className="w-4 h-5 cursor-pointer sm:h-9 size-4"
+              alt="backButton"
               onClick={handleClickBack}
             />
           </div>
           <div>
             <span className="text-[1.2rem] font-semibold">{title}</span>
           </div>
-          <div className="h-5 sm:h-9 size-4"></div>
-        </Navbar>
+          <div className="w-4 h-5 cursor-pointer sm:h-9 size-4"></div>
+        </div>
       </div>
       <Outlet context={{ setTitle }} />
     </div>
