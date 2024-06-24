5cf2e9e6b2c3def6885b814743b01d863c1abdd0
JungHeesoo <73633272+heeeesoo@users.noreply.github.com>
2024-06-24 10:53:15 +0900

    Revert "feat: like 조회"

--- src/components/mypage/FavoriteRooms.tsx
+++ src/components/mypage/FavoriteRooms.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import FavoriteRoomCard from './FavoriteRoomCard';
-import { PropertyI } from '@/routes/mypage/Mypage';
-type Props = { likeProperties: PropertyI[] };
+import React from "react";
+import FavoriteRoomCard from "./FavoriteRoomCard";
+type Props = {};
 
-export default function FavoriteRooms({ likeProperties }: Props) {
+const data = [1, 2, 3, 4, 5];
+
+export default function FavoriteRooms({}: Props) {
   return (
     <div className="w-[100%]">
       <div className="flex flex-row overflow-auto scroll-hidden">
-        {likeProperties.map((property: PropertyI, index: number) => {
+        {data.map((value, index) => {
           return (
-            <div key={property.id} className="mr-[0.4rem]">
-              <FavoriteRoomCard property={property} />
+            <div key={index} className="mr-[0.4rem]">
+              <FavoriteRoomCard />
             </div>
           );
         })}
--- src/routes/mypage/MypageProperties.tsx
+++ src/routes/mypage/MypageProperties.tsx
@@ -1,43 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
-import FavoriteDetailRoomCard from '../../components/mypage/FavoriteDetailRoomCard';
-import { getLikeProperties } from '@/lib/apis/property';
-import { PropertyI } from './Mypage';
+import React, { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+import FavoriteDetailRoomCard from "../../components/mypage/FavoriteDetailRoomCard";
 
 export default function MypageProperties() {
   const { setTitle } = useOutletContext<{
     setTitle: (title: string) => void;
   }>();
-  const [likeProperties, setLikeProperties] = useState<PropertyI[]>([]);
 
   useEffect(() => {
-    setTitle('찜한 방');
+    setTitle("찜한 방");
   }, [setTitle]);
 
-  const handleGetLikeProperties = async () => {
-    try {
-      const result = await getLikeProperties();
-      setLikeProperties(result.data.response);
-    } catch (error) {
-      console.log('[ERROR]:', error);
-    }
-  };
-
-  useEffect(() => {
-    handleGetLikeProperties();
-  }, []);
+  const data = [1, 2, 3, 4, 5, 6, 7, 10, 8, 9];
 
   return (
     <div className="pt-[80px] flex flex-col items-center">
       <div className=" w-customWidthPercent">
-        {likeProperties.length > 0 &&
-          likeProperties.map((property: PropertyI, index) => {
-            return (
-              <div key={property.id} className="mb-[0.7rem]">
-                <FavoriteDetailRoomCard property={property} />
-              </div>
-            );
-          })}
+        {data.map((value, index) => {
+          return (
+            <div key={index} className="pb-[0.5rem]">
+              <FavoriteDetailRoomCard />
+            </div>
+          );
+        })}
         <div className="py-[1rem]" />
       </div>
     </div>
