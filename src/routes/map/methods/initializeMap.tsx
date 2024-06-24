0d7dd3e3e590f893a538d976c4423ea83fd9f02d
김영석 <kimyoungseok15@gmail.com>
2024-06-24 12:25:59 +0900

    feat: 따옴표 바뀜

--- src/routes/map/methods/initializeMap.tsx
+++ src/routes/map/methods/initializeMap.tsx
@@ -1,7 +1,7 @@
-import { PropertyGroup, PropertyOption } from '@/utils/types';
-import { Dispatch, SetStateAction } from 'react';
-import { fetchPropertyGroups } from './fetchPropertyGroups';
-import { moveToCurrentLocation } from './moveToCurrentLocation';
+import { PropertyGroup, PropertyOption } from "@/utils/types";
+import { Dispatch, SetStateAction } from "react";
+import { fetchPropertyGroups } from "./fetchPropertyGroups";
+import { moveToCurrentLocation } from "./moveToCurrentLocation";
 
 export const initializeMap = (
   setPropertyGroups: (properties: PropertyGroup[]) => void,
@@ -24,7 +24,7 @@ export const initializeMap = (
   }
   kakao.maps.load(() => {
     // 지도 생성
-    const mapInstance = new kakao.maps.Map(container, options);
+    const mapInstance = new kakao.maps.Map(container, options); 
     setMap(mapInstance);
 
     // 편의시설 검색 객체 생성
@@ -36,26 +36,20 @@ export const initializeMap = (
     // 편의시설 상세정보 지우기
     const removeCovenientInfo = () => {
       setIsDrawerOpen(0);
-      if (customOverlayRef) {
-        customOverlayRef.current?.setMap(null);
-      }
+        if (customOverlayRef){
+          customOverlayRef.current?.setMap(null)
+        }
     };
 
     // 매물 가져오기
     fetchPropertyGroups(mapInstance, setPropertyGroups, propertyOption);
 
     // 현재 위치로 아이콘 부착
-    moveToCurrentLocation(mapInstance);
+    moveToCurrentLocation(mapInstance)
 
     // 줌, 항공뷰 컨트롤러 부착
-    mapInstance.addControl(
-      new kakao.maps.ZoomControl(),
-      kakao.maps.ControlPosition.RIGHT
-    );
-    mapInstance.addControl(
-      new kakao.maps.MapTypeControl(),
-      kakao.maps.ControlPosition.TOPRIGHT
-    );
+    mapInstance.addControl(new kakao.maps.ZoomControl(), kakao.maps.ControlPosition.RIGHT);
+    mapInstance.addControl(new kakao.maps.MapTypeControl(), kakao.maps.ControlPosition.TOPRIGHT);
 
     // 현재 '구' 가져오기
     const getGu = () => {
@@ -63,9 +57,14 @@ export const initializeMap = (
       geocoder.coord2RegionCode(center.getLng(), center.getLat(), (result, status) => {
         if (status === kakao.maps.services.Status.OK) {
           const gu = result[0].region_2depth_name;
+          const dong = result[0].region_3depth_name;
           const guCode = result[0].code.slice(0, 5);
           setGu(gu);
-          setGuCode(guCode)
+          console.log(gu, dong, guCode);
+          if (setAddress){
+            setAddress(gu+" "+dong);
+          }
+          setGuCode(guCode);
         } else {
           console.error('Geocoder failed due to: ' + status);
         }
@@ -76,14 +75,10 @@ export const initializeMap = (
     // 지도 이벤트 핸들링
     kakao.maps.event.addListener(mapInstance, 'click', removeCovenientInfo);
     kakao.maps.event.addListener(mapInstance, 'idle', getGu);
-
+    
     return () => {
-      kakao.maps.event.removeListener(
-        mapInstance,
-        'click',
-        removeCovenientInfo
-      );
-      kakao.maps.event.removeListener(mapInstance, 'idle', getGu);
+      kakao.maps.event.removeListener(mapInstance, 'click', removeCovenientInfo)
+      kakao.maps.event.removeListener(mapInstance, 'idle', getGu)
     };
   });
 };
